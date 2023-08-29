6734f9c7f92534e9e8f5f7ae318e2f93a58f90c1
Kamilla <113370998+Kanin60@users.noreply.github.com>
2023-08-29 12:40:56 +0200

    Button er lavet nu. Før brugte jeg en form.

--- src/components/SectionJoke/SectionJoke.jsx
+++ src/components/SectionJoke/SectionJoke.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState, useRef } from "react"
 
-export function Test() {
+export function SectionJoke() {
 
     const [joke, setJoke] = useState()
     const isExecuted = useRef(false)
 
+    function fetchJoke() {
+        const url = "https://v2.jokeapi.dev/joke/Programming"
+        fetch(url).then(res => res.json()).then(data => setJoke(data))
+    }
+    
     
     useEffect(() => {
         console.log("useEffect is in effect");
         if(isExecuted.current){
             return
-        }
-        async function fetchJoke() {
-            const url = "https://v2.jokeapi.dev/joke/Programming"
-            await fetch(url).then(res => res.json()).then(data => setJoke(data))
         }
         fetchJoke()
         isExecuted.current = true
@@ -23,12 +24,12 @@ export function Test() {
     console.log("Joke",joke)
 
     return(
-        <form>
-            <h1>Tryk for flere jokes</h1>
+        <div>
+            <h1>Programming jokes</h1>
             <h4>{joke && joke.joke}</h4>
             <h4>{joke && joke.setup}</h4>
             <p>{joke && joke.delivery}</p>
-            <button>Tryk her</button>
-        </form>
+            <button onClick={() => fetchJoke()}>Get a New Joke</button>
+        </div>
     )
 }
